3f054968b27c3a0303659e7e2f76a7b8027d58d6
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2025-02-22 20:22:41 +0200

    Tuple.java

---
+++ Tuple.java
@@ -0,0 +1,11 @@
+public class Tuple{
+    public Integer first;
+    public Integer second;
+    public char third;
+
+    public Tuple(Integer first, Integer second, char third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
