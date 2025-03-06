6e02edec8452ec334007d0571eaaca934af259bf
Ahmed Abbas <111700915+Ahmed-Abbas-2077@users.noreply.github.com>
2025-03-07 01:53:16 +0200

    '

--- SHuffman/SHuff.java
+++ SHuffman/SHuff.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SHuff {
     
@@ -26,8 +28,41 @@ public class SHuff {
     }
 
 
-    public static void write_txt (String bin_file){
-        
+    public static void write_txt (String txt_file, String content){
+        try {
+            Files.writeString(Paths.get(txt_file), content, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.err.println("Error writing to text file: " + e.getMessage());
+        }
+    }
+
+    public static void write_binary(String bin_file, byte[] data) {
+        try {
+            Files.write(Paths.get(bin_file), data);
+        } catch (IOException e) {
+            System.err.println("Error writing binary file: " + e.getMessage());
+        }
+    }
+
+
+    public static Map<Character, Integer> getFrequency(String text) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : text.toCharArray()){
+            frequencyMap.put(c, frequencyMap.getOrDefault(frequencyMap, 0) + 1);
+        }
+        return frequencyMap;
+    }
+
+    public static double calculateEntropy(String text){
+        Map<Character, Integer> freqMap = getFrequency(text);
+        double H = 0; // Entropy
+        int totalLength = text.length();
+
+        // calculating entropy in an iterative fashion
+        for (int freq : freqMap.values()){
+            double prob = (double) freq/totalLength; 
+
+        } 
     }
 
 }
